b0dc5abcc925d1f03d943a9854d705ad0c50db51
kodelm2168 <kodelm2168@student.faytechcc.edu>
2026-09-04 15:06:06 -0400

    My first python assignment
    Taking input from user and printing output

---
+++ P1LAB1_KodelMichael.py
@@ -0,0 +1,8 @@
+# Michael Kodel
+# 9/4/2026
+# P1LAB1
+# This program will ask the user for their name, then it will print a message with that information.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,",first_name,last_name +"!", "Welcome to CTI-110!")
